c6825981123ea2481dbb3505ecc2065d76e4f6e4
Peri Muhtaroglu <p.muhtaroglu@gmail.com>
2024-11-24 16:54:04 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,44 @@
+//NuggetGame Characters
+//backGround
+//yellowGuy
+//oppGuy
+//jibsGuy
+//nugget
+//beer
+
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1200, 675);
+  background(0, 200, 250);
 }
 
+function gameFloor(x, y) {
+  noStroke();
+  fill(0, 255, 0);
+  rect(x, y, 1200, 675);
+}
+
+function yellowGuy(x, y) {
+  push();
+  stroke(0);
+  strokeWeight(2);
+  fill(255, 255, 0);
+  square(x, y, 50);
+  pop();
+}
+
+function oppGuy(x, y) {
+  push();
+  stroke(0);
+  strokeWeight(2);
+  fill(0, 0, 255);
+  square(x, y, 50);
+  pop();
+}
+
+function jibsGuy(x, y) {}
+
 function draw() {
-  background(255, 140, 0);
+  gameFloor(0, 350);
+  yellowGuy(100, 300);
+  oppGuy(200, 300);
 }
